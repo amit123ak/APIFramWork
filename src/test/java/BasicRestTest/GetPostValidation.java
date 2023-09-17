9ed1b20144053365c2a7c38096a05eb9d26a4dbe
HP <HP@DESKTOP-DN37M11>
2023-09-17 12:42:27 +0530

    added postApiValidation function HashMap storing key value object mapper convert obect to json

--- src/test/java/BasicRestTest/GetPostValidation.java
+++ src/test/java/BasicRestTest/GetPostValidation.java
@@ -2,10 +2,18 @@ package BasicRestTest;
 
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
 import io.restassured.response.ResponseOptions;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetPostValidation {
 
@@ -18,6 +26,33 @@ public class GetPostValidation {
 		.log().all().get("users?page=2")
 		.then().statusCode(200)
 		.body("data[1].first_name", equalTo("Lindsay")).body("data.first_name", hasItems("Byron","George"));
+		
+	}
+	@Test
+	public void PostApiValidation() throws JsonProcessingException
+	{
+		Map<String,Object>map=new HashMap<>();
+		
+		map.put("name", "morpheus");
+		map.put("job", "leader");
+		ObjectMapper objectmapper=new ObjectMapper();
+		String  json=objectmapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+		//System.out.println("Hello");
+		//System.out.println(json);
+		
+           
+		baseURI="https://reqres.in/api";
+		given().log().all()
+		  .header("Content-Type","application/json")
+		  .contentType(ContentType.JSON)
+		  .accept(ContentType.JSON)
+		  .body(json)
+		  .when()
+		  .post("/users")
+		  .then().statusCode(201)
+		  
+		  .log().all();
+	
 		
 	}
 	
