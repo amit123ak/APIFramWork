b910e304817e4693fb29a3a6de6833e63c0de2a5
HP <HP@DESKTOP-DN37M11>
2023-09-17 10:44:35 +0530

    add TestNg Feature IN The API Validation

--- src/test/java/BasicRestTest/ListUserAPI.java
+++ src/test/java/BasicRestTest/ListUserAPI.java
@@ -1,26 +1,32 @@
 package BasicRestTest;
 
+import org.testng.annotations.Test;
+
 import io.restassured.RestAssured;
 import io.restassured.response.ResponseOptions;
 
 public class ListUserAPI {
 	
-	public static void main(String[] args) {
-	ResponseOptions response=	RestAssured.get("https://reqres.in/api/users?page=2");
-	
-	System.out.println(response.getStatusCode());
-	System.out.println(response.getTime());
-	
-	System.out.println(response.contentType());
-	
-	System.out.println(response.getBody().asString());
-	
-	System.out.println(response.getStatusLine());
-	System.out.println(response.getHeader("Content-Type"));
-	
-	
-	
+	@Test
+	public void Test1()
+	{
+		ResponseOptions response=	RestAssured.get("https://reqres.in/api/users?page=2");
+		
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		
+		System.out.println(response.contentType());
+		
+		System.out.println(response.getBody().asString());
+		
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("Content-Type"));
 		
 	}
-
+	     @Test
+         public void Test2()
+         {
+        	 
+        	 
+         }
 }
